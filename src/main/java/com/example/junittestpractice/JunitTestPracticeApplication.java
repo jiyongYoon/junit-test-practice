29f8e91654644cd3c070d689df02054c3a2844c8
jiyongYoon <jyyoon0615@naver.com>
2023-02-24 23:39:18 +0900

    init setting

---
+++ src/main/java/com/example/junittestpractice/JunitTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.junittestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitTestPracticeApplication.class, args);
+    }
+
+}
